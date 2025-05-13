0cb853f6b3feeb84badb8b3d1d956b0db7e28e98
Lav <laviniatecbx@gmail.com>
2025-05-13 20:44:05 -0300

    Add files via upload

---
+++ aula-18/script.js
@@ -0,0 +1,9 @@
+document.getElementById("tituloMundo").innerHTML = "Olá, Mundo!";
+        //document.writeln("<h3>Olá, Mundo!</h3>");
+        // window.alert("Olá, Mundo!");
+        // document.getElementById("resposta").innerHTML = window.confirm("Você está gostando do curso?");
+        // <!-- comandos de entrada -->
+
+        document.getElementById("resposta").innerHTML = "Meu nome é " + window.prompt("Qual é o seu nome?");
+
+        
---
+++ aula-18/script2.js
@@ -0,0 +1,6 @@
+function enviaDados(){
+    document.getElementById("inputUsuario").innerHTML = "O nome enviado foi: " + window.prompt("Digite seu nome, por gentileza: ");
+}
+function exibeDados(){
+    window.alert()
+}
